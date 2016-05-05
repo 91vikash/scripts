ed7a272ca37577c8243f8f8593849736608f22d6
Vikash <91.vikash@gmail.com>
2016-05-05 21:06:51 +0530

    Removed old directory structure

--- cookbooks/README.md
+++
@@ -1,3 +0,0 @@
-Openresty Cookbook
-
-
--- cookbooks/metadata.rb
+++
@@ -1,7 +0,0 @@
-name             'openresty'
-maintainer       'Vikash'
-maintainer_email '91.vikash@gmail.com'
-license          'All rights reserved'
-description      'Installs/Configures open-resty'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
